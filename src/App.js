ec308a356f55e7f4582b48b594c0e5bd7b0088e7
petgoldfish <grs348@nyu.edu>
2020-03-27 17:31:43 -0400

    Implemented fetching data from API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Card } from './components/Card/Card';
 
 function App() {
-	const [state, setState] = useState({
-		title: "Test Title",
-		summary: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut tenetur ratione perferendis veritatis voluptatibus ipsa quibusdam sit blanditiis incidunt deleniti pariatur maiores alias, a quisquam, nam exercitationem eaque, sapiente quae!",
-		imgSrc: "https://s3.reutersmedia.net/resources/r/?m=02&d=20200326&t=2&i=1508518991&w=1200&r=LYNXMPEG2P18P"
-	});
+	const [articles, setArticles] = useState([]);
+
+	useEffect(() => {
+		const fetchArticles = async () => {
+			const response = await axios.get(`https://newsapi.org/v2/top-headlines?sources=reuters&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`);
+			setArticles(response.data.articles);
+		};
+
+		fetchArticles();
+	}, []);
 
 	return (
 		<div>
-			<h1>Hello World!</h1>
-			<Card data={state} />
+			{articles.map((article, index) => {
+				return <Card key={index} article={article} />
+			})}
 		</div>
 	);
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './Card.css';
 
 export const Card = props => {
-	const article = { ...props.data };
+	const article = { ...props.article };
 	return (
 		<div className="card">
-			<img className="card__image" src={article.imgSrc} />
+			<img className="card__image" src={article.urlToImage} />
 			<div className="card-text">
 				<div className="card-text__title">
 					{article.title}
 				</div>
 				<div className="card-text__summary">
-					{article.summary}
+					{article.description}
 				</div>
 			</div>
 		</div>
